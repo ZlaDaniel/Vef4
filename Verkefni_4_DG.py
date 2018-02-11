7ab4157cc13fb018df89acf771879782a9657b5a
ZlaDaniel <danni539953@gmail.com>
2018-02-11 20:42:40 +0000

    Update Verkefni_4_DG.py

--- Verkefni_4_DG.py
+++ Verkefni_4_DG.py
@@ -1,4 +1,5 @@
 from bottle import route, run, error, static_file, template
+import os
 
 import json
 """
@@ -31,4 +32,4 @@ def error404(error):
 def error404(error):
     return "Þessi síða er ekki til"
 
-run()
+run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
